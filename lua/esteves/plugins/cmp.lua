ff4e057c614bf56d03bfd548984302b4582fab23
estevesnp <nuno.pinto@live.com>
2024-12-03 00:14:29 +0000

    added zig snippet

--- lua/esteves/plugins/cmp.lua
+++ lua/esteves/plugins/cmp.lua
@@ -31,6 +31,7 @@ return {
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 		luasnip.config.setup({})
+		require("luasnip.loaders.from_snipmate").load()
 
 		cmp.setup({
 			snippet = {
