8745c124507937f9c71fe90d728d36d4bfdd1913
estevesnp <nuno.pinto@live.com>
2024-12-21 09:52:58 +0000

    added zig mapping

--- ftplugin/zig.lua
+++ ftplugin/zig.lua
@@ -1,1 +1,12 @@
 vim.g.zig_fmt_parse_errors = 0
+
+local map = require("config.utils").namespaced_keymap("Zig")
+
+local ignore_under_cursor = function()
+	local word = vim.fn.expandcmd("<cword>")
+	local raw_keys = "o_ = " .. word .. ";<ESC>"
+	local keys = vim.api.nvim_replace_termcodes(raw_keys, true, false, true)
+	vim.api.nvim_feedkeys(keys, "n", false)
+end
+
+map("n", "<leader>zi", ignore_under_cursor, "Ignore word under cursor")
