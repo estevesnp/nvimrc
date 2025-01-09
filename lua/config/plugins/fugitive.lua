55391cbc4894183c2790b3b53ef174085d3e645d
estevesnp <nuno.pinto@live.com>
2025-01-09 23:32:51 +0000

    added prettierd to j/tsx, renamed fugitive keymap

--- lua/config/plugins/fugitive.lua
+++ lua/config/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
 		"tpope/vim-fugitive",
 		config = function()
 			local map = require("config.utils").namespaced_keymap("Fugitive")
-			map("n", "<leader>fg", "<cmd>Git<CR>", "Open [f]u[g]itive")
+			map("n", "<leader>gi", "<cmd>Git<CR>", "Open fu[gi]tive")
 		end,
 	},
 }
