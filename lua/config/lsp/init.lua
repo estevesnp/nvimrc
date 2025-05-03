66dd15d1b032a5add21d679867a13e2f39240afc
estevesnp <nuno.pinto@live.com>
2025-05-03 02:04:30 +0100

    swapped cmp for blink

---
+++ lua/config/lsp/init.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+M.servers = require("config.lsp.servers")
+
+function M.setup()
+	require("config.lsp.autocmds").setup()
+	require("config.lsp.keymaps").setup()
+end
+
+return M
---
+++ lua/config/plugins/blink.lua
@@ -0,0 +1,55 @@
+return {
+	{
+		"saghen/blink.cmp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+
+		version = "1.*",
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+
+			completion = {
+				documentation = {
+					auto_show = false,
+				},
+				menu = {
+					max_height = 25,
+				},
+			},
+
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+
+				providers = {
+					buffer = {
+						min_keyword_length = 4,
+					},
+				},
+			},
+
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = {
+				preset = "none",
+				["<C-f>"] = { "accept" },
+				["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+				["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+				["<C-k>"] = { "show", "show_documentation", "hide_documentation" },
+				["<M-u>"] = { "scroll_documentation_up", "fallback" },
+				["<M-d>"] = { "scroll_documentation_down", "fallback" },
+
+				["<C-l>"] = { "snippet_forward", "fallback" },
+				["<C-h>"] = { "snippet_backward", "fallback" },
+
+				["<C-q>"] = { "hide" },
+			},
+		},
+		opts_extend = { "sources.default" },
+	},
+}
