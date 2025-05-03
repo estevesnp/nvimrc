ad10f5e1a838e8fec45d37bacb83e9bab2aee6c0
estevesnp <nuno.pinto@live.com>
2025-05-03 02:04:30 +0100

    swapped cmp for blink

--- lua/config/plugins/lsp-config.lua
+++ lua/config/plugins/lsp-config.lua
@@ -6,8 +6,7 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-nvim-lsp",
+			"saghen/blink.cmp",
 
 			-- LSP Status Updates
 			{ "j-hui/fidget.nvim", opts = {} },
@@ -26,21 +25,18 @@ return {
 			},
 		},
 		config = function()
-			require("config.lsp.autocmds").setup()
-
-			local servers = require("config.lsp.servers")
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			local lsp = require("config.lsp")
+			lsp.setup()
 
 			require("mason").setup()
-			require("mason-tool-installer").setup({ ensure_installed = servers.ensure_installed })
+			require("mason-tool-installer").setup({ ensure_installed = lsp.servers.ensure_installed })
 			require("mason-lspconfig").setup({
 				ensure_installed = nil,
 				automatic_installation = false,
 				handlers = {
 					function(server_name)
-						local server = servers.configs[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						local server = lsp.servers.configs[server_name] or {}
+						server.capabilities = require("blink.cmp").get_lsp_capabilities(server.capabilities or {})
 						require("lspconfig")[server_name].setup(server)
 					end,
 				},
