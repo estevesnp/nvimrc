02391c797a90f48351de8355de281d96cce2571f
estevesnp <nuno.pinto@live.com>
2025-05-12 16:25:35 +0100

    unhide git in oil

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,33 +1,36 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter-context",
-
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			build = ":TSUpdate",
-			opts = {
-				ensure_installed = {
-					"go",
-					"zig",
-					"rust",
-					"c",
-					"html",
-					"lua",
-					"vim",
-					"vimdoc",
-					"diff",
-					"git_rebase",
-					"gitcommit",
-				},
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"go",
+				"zig",
+				"rust",
+				"c",
+				"html",
+				"lua",
+				"vim",
+				"vimdoc",
+				"diff",
+				"git_rebase",
+				"gitcommit",
 			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
 		},
 
 		config = function(_, opts)
 			require("nvim-treesitter.install").prefer_git = true
 			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
 			require("treesitter-context").setup({
 				enable = true,
 				max_lines = 0,
