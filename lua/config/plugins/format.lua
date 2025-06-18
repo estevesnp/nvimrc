9ef1f1738363bbe2d9f2549d65a2f9e31a35decd
estevesnp <nuno.pinto@live.com>
2025-06-18 17:11:12 +0100

    add print messages for `FormatEnable` and `FormatDisable`

--- lua/config/plugins/format.lua
+++ lua/config/plugins/format.lua
@@ -30,8 +30,10 @@ return {
       if args.bang then
         -- FormatDisable! will disable formatting just for this buffer
         vim.b.disable_autoformat = true
+        print("Disabled autoformat-on-save for the current buffer")
       else
         vim.g.disable_autoformat = true
+        print("Disabled autoformat-on-save")
       end
     end, {
       desc = "Disable autoformat-on-save",
@@ -40,6 +42,7 @@ return {
     vim.api.nvim_create_user_command("FormatEnable", function()
       vim.b.disable_autoformat = false
       vim.g.disable_autoformat = false
+      print("Enabled autoformat-on-save")
     end, {
       desc = "Re-enable autoformat-on-save",
     })
