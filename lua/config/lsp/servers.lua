15d8c139f66ae784cf164aa4148e2a954cf497e7
estevesnp <nuno.pinto@live.com>
2025-09-01 15:09:40 +0100

    fix: temporary fix for single-file js/ts support

--- lua/config/lsp/servers.lua
+++ lua/config/lsp/servers.lua
@@ -51,6 +51,20 @@ M.configs = {
     cmd = { "clangd", "--offset-encoding=utf-16", "--function-arg-placeholders=0" },
   },
 
+  -- TODO: this is a temporary fix for single-file support. tracked at https://github.com/neovim/nvim-lspconfig/issues/4015
+  ts_ls = {
+    root_dir = function(bufnr, on_dir)
+      local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }
+      root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers } or root_markers
+      local project_root = vim.fs.root(bufnr, root_markers)
+      if not project_root then
+        on_dir(nil)
+      else
+        on_dir(project_root)
+      end
+    end,
+  },
+
   bashls = {
     filetypes = { "sh", "bash", "zsh", "zshrc" },
   },
