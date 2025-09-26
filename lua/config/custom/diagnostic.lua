1eac4785b5650e38afc35253c2343606f28f3b7d
estevesnp <nuno.pinto@live.com>
2025-09-26 07:37:14 +0100

    feat: improve diagnostics toggle

--- lua/config/custom/diagnostic.lua
+++ lua/config/custom/diagnostic.lua
@@ -1,11 +1,42 @@
-local show_virtual_text = true
-local show_virtual_lines = not show_virtual_text
+local diagnostic_configs = {
+  {
+    message = "Default",
+    config = {
+      virtual_lines = false,
+      virtual_text = true,
+      underline = true,
+    },
+  },
+  {
+    message = "Virtual Lines",
+    config = {
+      virtual_lines = true,
+      virtual_text = false,
+      underline = true,
+    },
+  },
+  {
+    message = "Only Display Errors",
+    config = {
+      virtual_lines = false,
+      virtual_text = {
+        severity = { min = vim.diagnostic.severity.ERROR },
+      },
+      underline = {
+        severity = { min = vim.diagnostic.severity.ERROR },
+      },
+    },
+  },
+}
+
+local counter = 1
+local max_value = #diagnostic_configs
 
 return function()
-  show_virtual_text = not show_virtual_text
-  show_virtual_lines = not show_virtual_lines
-  vim.diagnostic.config({
-    virtual_text = show_virtual_text,
-    virtual_lines = show_virtual_lines,
-  })
+  counter = (counter % max_value) + 1
+  local diag_config = diagnostic_configs[counter]
+
+  print("Diagnostic Setting: " .. diag_config.message)
+
+  vim.diagnostic.config(diag_config.config)
 end
