2580db8083a0e8aee080b2334854e76d58f8e73e
estevesnp <nuno.pinto@live.com>
2026-01-04 15:56:59 +0000

    feat: implemented mason and lsp mappings

--- lua/config/lsp/mason-mappings.lua
+++ lua/config/lsp/mason-mappings.lua
@@ -1,6 +1,9 @@
+local mason_cmd = require("mason.api.command")
+
 local M = {}
 
 local cache = {
+  ---@type LspMappings|nil
   mappings = nil,
 }
 
@@ -11,15 +14,97 @@ function M.reset_cache()
   end
 end
 
+---@class LspMappings
+---@field mason_to_lspconfig table<string, string>
+---@field lspconfig_to_mason table<string, string>
+
+---@return LspMappings
 local function get_mappings()
-  -- TODO - implement basing on https://github.com/mason-org/mason-lspconfig.nvim/blob/main/lua/mason-lspconfig/mappings.lua
+  local mason_to_lspconfig = {}
+  local lspconfig_to_mason = {}
+
+  local specs = require("mason-registry").get_all_package_specs()
+
+  for _, spec in ipairs(specs) do
+    local mason = spec.name
+    local lspconfig = vim.tbl_get(spec, "neovim", "lspconfig")
+
+    mason_to_lspconfig[mason] = lspconfig
+    if lspconfig then
+      lspconfig_to_mason[lspconfig] = mason
+    end
+  end
+
+  return {
+    mason_to_lspconfig = mason_to_lspconfig,
+    lspconfig_to_mason = lspconfig_to_mason,
+  }
 end
 
+---get lsp mappings between mason and lspconfig
+---mason plugin must have already been loaded
+---@return LspMappings
 function M.get_mappings()
   if cache.mappings == nil then
     cache.mappings = get_mappings()
   end
   return cache.mappings
+end
+
+---get the mapping for an lsp from mason to lspconfig
+---mason plugin must have already been loaded
+---@param mason_name string
+---@return string|nil
+function M.mason_to_lspconfig(mason_name)
+  return M.get_mappings().mason_to_lspconfig[mason_name]
+end
+
+---get the mapping for an lsp from mason to lspconfig
+---mason plugin must have already been loaded
+---@param lspconfig_name string
+---@return string|nil
+function M.lspconfig_to_mason(lspconfig_name)
+  return M.get_mappings().lspconfig_to_mason[lspconfig_name]
+end
+
+---install provided servers with MasonInstall
+---mason plugin must have already been loaded
+---@param servers string|string[]
+function M.mason_install(servers)
+  servers = vim._ensure_list(servers) --[[@as string[] ]]
+
+  local mappings = M.get_mappings()
+
+  local servers_to_install = {}
+  local skipped_servers = {}
+
+  for _, server in ipairs(servers) do
+    local server_to_install = server
+
+    if vim.fn.has_key(mappings.mason_to_lspconfig, server_to_install) == 0 then
+      print("no match, fetching alt")
+      server_to_install = mappings.lspconfig_to_mason[server]
+    end
+
+    if server_to_install then
+      print("installing " .. server_to_install)
+      table.insert(servers_to_install, server_to_install)
+    else
+      table.insert(skipped_servers, server)
+    end
+  end
+
+  if #skipped_servers > 0 then
+    vim.notify(
+      "couldn't get mappings for servers, skipping: " .. table.concat(skipped_servers, ", "),
+      vim.log.levels.WARN
+    )
+  end
+
+  if #servers_to_install == 0 then
+    return
+  end
+  mason_cmd.MasonInstall(servers_to_install)
 end
 
 return M
