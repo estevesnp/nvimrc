d944a28ddb3460adf0471700dab52c57a6d375f5
estevesnp <nuno.pinto@live.com>
2026-03-08 00:07:25 +0000

    chore: simplify highlight autocmd

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -45,7 +45,6 @@ vim.o.winborder = "single"
 require("config.diagnostics").set_default_diagnostic()
 
 -- folds
-vim.o.foldenable = false
 vim.o.foldlevel = 99
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
