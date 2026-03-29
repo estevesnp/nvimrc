66ee2c70c04195a7d790e79e0e02daf87df6dc38
estevesnp <nuno.pinto@live.com>
2026-03-29 00:18:42 +0000

    chore(undotree): re-use require

--- plugin/undotree.lua
+++ plugin/undotree.lua
@@ -2,7 +2,9 @@ vim.pack.add({
   "https://github.com/jiaoshijie/undotree",
 })
 
-require("undotree").setup({})
+local UndoTree = require("undotree")
+UndoTree.setup({})
 
 local map = require("config.utils").namespaced_keymap("undotree")
-map("n", "<leader>u", require("undotree").toggle, "toggle undo tree")
+
+map("n", "<leader>u", UndoTree.toggle, "toggle undo tree")
