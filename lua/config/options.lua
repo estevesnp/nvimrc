52c7fc2dd27577f01c91bcbb3e65e775b4a31b30
estevesnp <nuno.pinto@live.com>
2026-05-26 16:09:58 +0100

    chore: replace deprecated vim.hl.on_yank()

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -59,7 +59,7 @@ vim.o.undofile = true
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+    vim.hl.hl_op()
   end,
 })
 
