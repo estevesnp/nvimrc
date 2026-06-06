d53012959e7e1f73e8f8fcfa444a1172bd6528f8
estevesnp <nuno.pinto@live.com>
2026-06-06 21:29:10 +0100

    feat: add debugprint

---
+++ plugin/debugprint.lua
@@ -0,0 +1,5 @@
+vim.pack.add({ "https://github.com/andrewferrier/debugprint.nvim" })
+require("debugprint").setup()
+
+-- g?p -> print location below, P for above, sp for both
+-- g?v -> print variable below, V for above, sv for both
