babbe21eacd68adb4beb8a5d840e19faa0af575c
estevesnp <nuno.pinto@live.com>
2026-06-18 08:26:15 +0100

    feat(debugprint): remove odin debug (merged)

--- plugin/debugprint.lua
+++ plugin/debugprint.lua
@@ -3,13 +3,3 @@ require("debugprint").setup()
 
 -- g?p -> print location below, P for above, sp for both
 -- g?v -> print variable below, V for above, sv for both
-
-require("debugprint").add_custom_filetypes({
-  odin = {
-    left = 'fmt.eprintfln("',
-    right = '", #location())',
-    mid_var = '%v", #location(), ',
-    location = "%s",
-    right_var = ")",
-  },
-})
