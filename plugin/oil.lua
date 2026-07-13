859a2bd28682593cc82d53ed659523518b1ed6f1
estevesnp <nuno.pinto@live.com>
2026-07-13 17:06:26 +0100

    feat: use canola instead of oil

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/barrettruth/canola.nvim", -- fork of stevearc/oil.nvim
   "https://github.com/refractalize/oil-git-status.nvim",
 })
 
