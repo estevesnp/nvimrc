4ac9ec1df3434d532ed9b1c0fedd2dcef853ea68
estevesnp <nuno.pinto@live.com>
2026-09-13 23:38:24 +0100

    feat: remove fugitive bind

--- plugin/tpope.lua
+++ plugin/tpope.lua
@@ -2,6 +2,3 @@ vim.pack.add({
   "https://github.com/tpope/vim-surround",
   "https://github.com/tpope/vim-fugitive",
 })
-
-local map = require("config.utils").namespaced_keymap("fugitive")
-map("n", "<leader>fg", "<cmd>Git<CR>", "open fugitive")
